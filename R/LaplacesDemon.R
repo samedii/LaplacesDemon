1988b261dd3252b1ee52c3e9564c395d0cb23aaa
Richard Hermanson <samedii@gmail.com>
2015-07-15 10:14:23 +0200

    Added optional Bounds argument to IM
    Algorithm is incorrect otherwise if advertised function "interval" is used. Note that Bounds adds many density calculations and will slow down the algorithm significantly

--- R/LaplacesDemon.R
+++ R/LaplacesDemon.R
@@ -4773,10 +4773,61 @@ LaplacesDemon <- function(Model, Data, Initial.Values, Covar=NULL,
      Debug, LogFile)
      {
      mu <- Specs[["mu"]]
-     VarCov <- as.positive.definite(as.symmetric.matrix(VarCov * 1.1))
+     Bounds <- Specs[["Bounds"]]
+     VarCov <- as.positive.definite(as.symmetric.matrix(VarCov))
      Omega <- as.inverse(VarCov)
      U <- chol(VarCov)
      d <- eigen(VarCov, symmetric=TRUE)$values
+     e <- .Machine$double.eps^0.5
+     .dmvn <- function(x,mu,Omega) {
+          ss <- x - mu
+          z <- rowSums({ss %*% Omega} * ss)
+          exp(sum(-0.5 * (LIV * log(2*pi) + sum(log(d))) - (0.5*z)))}
+     .dmvnInterval <- function(x,mu,Omega,iterVariable=1) {
+          i <- iterVariable
+          l <- 0
+          a <- Bounds[[i]][1]
+          b <- Bounds[[i]][2]
+          #Current
+          if(i==LIV)
+               l <- l + .dmvn(x,mu,Omega)
+          else
+               l <- l + .dmvnInterval(x,mu,Omega,i+1)
+          #Start plus
+          conditionsFulfilled <- 0
+          n <- 1
+          xPlus <- x
+          repeat {
+               if(n %% 2 == 0) xPlus[i] <- 2*a-xPlus[i]
+               else xPlus[i] <- 2*b-xPlus[i]
+               if(!is.finite(xPlus[i])) break
+               .l <- 0
+               if(i>=LIV)
+                    .l <- .dmvn(xPlus,mu,Omega)
+               else
+                    .l <- .dmvnInterval(xPlus,mu,Omega,i+1)
+               l <- l+.l
+               if(.l < e) conditionsFulfilled<-conditionsFulfilled+1
+               if(conditionsFulfilled >= 2) break
+               n<-n+1}
+          #Start minus
+          conditionsFulfilled <- 0
+          n <- -1
+          xMinus <- x
+          repeat {
+               if(n %% 2 == 0) xMinus[i] <- 2*b-xMinus[i]
+               else xMinus[i] <- 2*a-xMinus[i]
+               if(!is.finite(xMinus[i])) break
+               .l <- 0
+               if(i>=LIV)
+                    .l <- .dmvn(xMinus,mu,Omega)
+               else
+                    .l <- .dmvnInterval(xMinus,mu,Omega,i+1)
+               l <- l+.l
+               if(.l < e) conditionsFulfilled<-conditionsFulfilled+1
+               if(conditionsFulfilled >= 2) break
+               n<-n-1}
+          l}
      for (iter in 1:Iterations) {
           ### Print Status
           if(iter %% Status == 0)
@@ -4784,7 +4835,7 @@ LaplacesDemon <- function(Model, Data, Initial.Values, Covar=NULL,
           ### Propose new parameter values
           MVNz <- try(rbind(rnorm(LIV)) %*% U, silent=TRUE)
           if(!inherits(MVNz, "try-error")) {
-               if(iter %% Status == 0) 
+               if(iter %% Status == 0)
                    cat(",   Proposal: Multivariate,   LP: ",
                         round(Mo0[["LP"]],1), "\n", sep="",
                         file=LogFile, append=TRUE)
@@ -4799,8 +4850,7 @@ LaplacesDemon <- function(Model, Data, Initial.Values, Covar=NULL,
                     cat("  Iteration:", iter, "Proposal:\n",
                          paste("c(",paste(prop, collapse=","),")",
                          sep=""), "\n", file=LogFile, append=TRUE)}
-               Mo1 <- Mo0
-               }
+               Mo1 <- Mo0}
           else if(any(!is.finite(c(Mo1[["LP"]], Mo1[["Dev"]],
                Mo1[["Monitor"]])))) {
                if(Debug[["DB.Model"]] == TRUE) {
@@ -4809,22 +4859,17 @@ LaplacesDemon <- function(Model, Data, Initial.Values, Covar=NULL,
                     cat("  Iteration:", iter, "Proposal:\n",
                          paste("c(",paste(prop, collapse=","),")",
                          sep=""), "\n", file=LogFile, append=TRUE)}
-               Mo1 <- Mo0
-               }
-          ### Importance Densities (dmvn)
-          ss <- prop - mu
-          z <- rowSums({ss %*% Omega} * ss)
-          d1 <- sum(-0.5 * (LIV * log(2*pi) + sum(log(d))) - (0.5*z))
-          ss <- Mo0[["parm"]] - mu
-          z <- rowSums({ss %*% Omega} * ss)
-          d0 <- sum(-0.5 * (LIV * log(2*pi) + sum(log(d))) - (0.5*z))
-          ### Accept/Reject
-          log.u <- log(runif(1))
-          log.alpha <- Mo1[["LP"]] - Mo0[["LP"]] + d1 - d0
-          if(!is.finite(log.alpha)) log.alpha <- 0
-          if(log.u < log.alpha) {
-               Mo0 <- Mo1
-               Acceptance <- Acceptance + 1}
+               Mo1 <- Mo0}
+          else {
+               ### Importance Densities (dmvn)
+               d1 <- log(.dmvnInterval(prop,mu,Omega))
+               d0 <- log(.dmvnInterval(Mo0[["parm"]],mu,Omega))
+               ### Accept/Reject
+               log.u <- log(runif(1))
+               log.alpha <- Mo1[["LP"]] - Mo0[["LP"]] + d1 - d0
+               if(log.u < log.alpha && is.finite(log.alpha)) {
+                    Mo0 <- Mo1
+                    Acceptance <- Acceptance + 1}}
           ### Save Thinned Samples
           if(iter %% Thinning == 0) {
                t.iter <- floor(iter / Thinning) + 1
